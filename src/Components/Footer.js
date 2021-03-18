d499212ae128f3cb6b32bda9b3eb9412daee91a6
Joan <joan.frey68@gmail.com>
2021-03-18 09:57:29 +0100

    footer text + background testimonials

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021 Joan Frey</li>
+              <li>Design by <a title="Joan" href="https://joan-frey.com/">Joan Frey</a></li>
            </ul>
 
         </div>
