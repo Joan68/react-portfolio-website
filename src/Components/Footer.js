89838ff09704e199e01f725cc51e9999c418ce82
Joan68 <joan.frey68@gmail.com>
2021-03-19 12:50:46 -0700

    style update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,6 +21,7 @@ class Footer extends Component {
            <ul className="copyright">
               <li>&copy; Copyright 2021 Joan Frey</li>
               <li>Design by <a title="Joan" href="https://joan-frey.com/">Joan Frey</a></li>
+              <li>Icon of the website made By Freepik</li>
            </ul>
 
         </div>
