0e95611703701fc3fa7d6e6dcd8074a5ec4cad93
Joan68 <joan.frey68@gmail.com>
2021-04-22 08:55:30 -0700

    text faults correction

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -18,8 +18,8 @@ const isLocalhost = Boolean(
     )
 );
 
-export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+export function register() {
+  if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
@@ -40,8 +40,8 @@ export default function register() {
         registerValidSW(swUrl);
       }
     });
-  }
-}
+  };
+};
 
 function registerValidSW(swUrl) {
   navigator.serviceWorker
